8e4d005c9bf70602195c742ffbb6ddfc2cadc365
juleari <juleari@yandex.ru>
2016-04-08 06:51:47 +0300

    semantic ok for day-of-week

--- dev/semantic.scm
+++ dev/semantic.scm
@@ -76,6 +76,17 @@
 
 (define R-NAME 0)
 (define R-TERMS 1)
+
+(define TYPE-ARGS 0)
+(define TYPE-ARGS-NUM 0)
+(define TYPE-ARGS-CHECK 1)
+(define TYPE-ARGS-NAMES 2)
+(define TYPE-DEFS 1)
+(define TYPE-EXPRS 2)
+(define TYPE-ARGS-NAMES 2)
+
+(define ERROR_NUM_OF_ARGS "the expected number of arguments does not match the given number")
+(define ERROR_UNDEFINED_VARIABLE "cannot reference undefined identifier")
 ;; end defs
 
 ;; lib
@@ -93,6 +104,14 @@
 (define-syntax cdns
   (syntax-rules ()
     ((_ x xs) (append xs (list x)))))
+
+(define (filter pred? xs)
+  (if (null? xs)
+      '()
+      (let ((x (car xs)))
+        (if (pred? x)
+            (cons x (filter pred? (cdr xs)))
+            (filter pred? (cdr xs))))))
 
 (define (get-token-tag token)
   (vector-ref token T-TAG))
@@ -146,7 +165,7 @@
   (let* ((terms (get-rule-terms arg-rule))
          (first-term (car terms))
          (first-type (get-rule-name first-term)))
-    (cond ((eq? first-type 'simple-argument) (string->symbol (get-simple-arg-name first-term))))))
+    (cond ((eq? first-type 'simple-argument) (get-simple-arg-name first-term)))))
 
 (define (get-names-of-args func-args)
   (map get-name-of-arg func-args))
@@ -163,10 +182,6 @@
             (lambda (x) (>= x (- num 1)))
             (lambda (x) (eq? x num))))))
 
-(define (get-body-list func-def model)
-  (let ((b-list (cddr (get-rule-terms func-def))))
-    (semantic b-list (cons '() model) '())))
-
 (define (add-func-in-model model name obj)
   (cons (list name obj) model))
 
@@ -177,47 +192,155 @@
     (if (equal? p-name name)
         (cons (list name (cdns val p-vals)) (cdr model)))))
 
-(define (get-from-model func-name model)
+#|find-in model symbol-func-name '<model>|#
+(define (find-in-model func-name model)
   (and (not-null? model)
-       (or (assoc func-name (car model))
-           (get-from-model func-name (cdr model)))))
+       ;(print 'find-in-model func-name (not (not (assoc func-name (car model)))) (car model))
+       (let ((car-model (car model)))
+         (or (assoc func-name car-model)
+             (and (find-in-params func-name car-model)
+                  (print '!!!in-params!!!)
+                  func-name)
+             (find-in-model func-name (cdr model))))))
+
+(define (get-args-from-type type)
+  (vector-ref type TYPE-ARGS))
+
+(define (get-args-num-from-type type)
+  (vector-ref (get-args-from-type type) TYPE-ARGS-NUM))
+
+(define (get-args-names-from-type type)
+  (vector-ref (get-args-from-type type) TYPE-ARGS-NAMES))
+
+(define (x-in-xs x xs)
+  (and (not-null? xs)
+       (or (equal? x (car xs))
+           (x-in-xs x (cdr xs)))))
+
+(define (find-in-params func-name model)
+  (and (not-null? model)
+       (let* ((type (car model))
+              (get-args-names-from-type type))
+         (x-in-xs func-name get-args-names-from-type))))
+
+(define (make-arg-type)
+  (vector (vector (lambda (x) (zero? x)) (list) (list)) (list) (list)))
+
+(define (make-alist xs)
+  (define (helper xs alist)
+    (if (null? xs)
+        alist
+        (helper (cdr xs) (cons (list (car xs) (make-arg-type)) alist))))
+  (helper xs '()))
 ;; end lib
 
-(define (semantic-func-def func-def model)
-  (let* ((func-decl (get-decl-from-def func-def))
-         
-         (func-name-token (get-token-name-from-decl func-decl))
-         (func-name (string->symbol (get-token-value func-name-token)))
-         (in-model (get-from-model func-name model))
-         (this-model (car model))
-         
-         (func-args (get-args-from-decl func-decl))
-         (num-of-args (get-num-of-args func-args))
-         (types-of-args (get-types-of-args func-args))
-         (names-of-args (get-names-of-args func-args))
-         (args-vector (vector num-of-args types-of-args names-of-args))
-         
-         (body-list (get-body-list func-def model))
-         (f-defs (car body-list))
-         (f-exprs (cadr body-list))
-         
-         (func-val (vector args-vector f-defs f-exprs))
-         (add-list (list this-model func-name func-val)))
-    (cons (apply (if in-model
-                     add-type-in-model
-                     add-func-in-model)
-                 add-list)
-          (cdr model))))
-
-(define (semantic-expr rule model)
-  rule)
-
-(define (semantic ast model exprs)
-  (print "semantic" ast model)
-  (if (null? ast)
-      (list (reverse (car model)) (reverse exprs))
-;      model
-      (let* ((rule (car ast))
-             (name (get-rule-name rule)))
-        (cond ((eq? name 'func-def) (semantic (cdr ast) (semantic-func-def rule model) exprs))
-              ((eq? name 'expr) (semantic (cdr ast) model (cons (semantic-expr rule model) exprs)))))))
+#|
+<model> = ((<name> (#(<#args> '<model> '<expr>)
+                    #(<#args> '<model> '<expr>))))
+<#args> = #(<lambda:num-of-args> '<lambda:type-of-arg> '<name>)
+|#
+(define semantic
+  (let ((errors '()))
+    (lambda (ast)
+      
+      (define (add-error error-type token)
+        (set! errors
+              (cons (vector 'error: token '- error-type)
+                    errors))
+        '())
+      
+      (define (add-error-rule rule token)
+        (set! errors
+              (cons (vector 'error: (get-token-coords token) '- 'expected `,rule)
+                    errors))
+        '())
+      
+      (define (print-errors)
+        (if (null? errors)
+            (display "SEMANTIC OK!")
+            (begin (display "SEMANTIC ERRORS:")
+                   (newline)
+                   (apply print (reverse errors))))
+        (newline)
+        (newline))
+      
+      (define (semantic-func-body func-def-terms names-of-args model)
+        (let ((b-list (cddr func-def-terms)))
+          (semantic-program b-list (cons (make-alist names-of-args) model) '())))
+      
+      (define (semantic-func-def func-def-terms model)
+        (let* ((func-decl (car func-def-terms))
+               
+               (func-name-token (get-token-name-from-decl func-decl))
+               (func-name (get-token-value func-name-token))
+               (in-model (find-in-model func-name model))
+               (this-model (car model))
+               
+               (func-args (get-args-from-decl func-decl))
+               (num-of-args (get-num-of-args func-args))
+               (types-of-args (get-types-of-args func-args))
+               (names-of-args (get-names-of-args func-args))
+               (args-vector (vector num-of-args types-of-args names-of-args))
+               
+               (body-list (semantic-func-body func-def-terms names-of-args model))
+               (f-defs (car body-list))
+               (f-exprs (cadr body-list))
+               
+               (func-val (vector args-vector f-defs f-exprs))
+               (add-list (list this-model func-name func-val)))
+          (cons (apply (if in-model
+                           add-type-in-model
+                           add-func-in-model)
+                       add-list)
+                (cdr model))))
+      
+      (define (semantic-func-call name-token args func-types)
+        (let* ((name (get-token-value name-token))
+               (arg-len (length args))
+               (correct-types (filter (lambda (type)
+                                        ((get-args-num-from-type type) arg-len))
+                                      func-types)))
+          (or (and (null? correct-types)
+                   (add-error ERROR_NUM_OF_ARGS name-token))
+              (and (zero? arg-len)
+                   name)
+              (cons name (map get-simple-arg-name args)))))
+      
+      (define (semantic-var func-decl-terms model)
+        (let* ((s-rule (car func-decl-terms))
+               (name-token (get-token-from-simple-rule s-rule))
+               (name (get-token-value name-token))
+               (args (cdr func-decl-terms))
+               (in-model (find-in-model name model)))
+          ;(print name in-model model)
+          (or (and in-model
+                   (semantic-func-call name-token args (cdr in-model)))
+              (and (add-error ERROR_UNDEFINED_VARIABLE name-token)
+                   func-decl-terms))))
+      
+      (define (semantic-expr-elem elem model)
+        (let ((type (get-rule-name elem)))
+          (if (eq? type 'func-decl)
+              (semantic-var (get-rule-terms elem) model)
+              (get-token-value elem))))
+      
+      (define (semantic-expr terms model)
+        (map (lambda (x) (semantic-expr-elem x model)) terms))
+      
+      (define (semantic-program ast model exprs)
+        (if (null? ast)
+            (list (reverse (car model)) (reverse exprs))
+            (let* ((rule (car ast))
+                   (name (get-rule-name rule))
+                   (terms (get-rule-terms rule)))
+              (cond ((eq? name 'func-def)
+                     (semantic-program (cdr ast)
+                                       (semantic-func-def terms model)
+                                       exprs))
+                    ((eq? name 'expr)
+                     (semantic-program (cdr ast)
+                                       model
+                                       (cons (semantic-expr terms model) exprs)))))))
+      
+      (let ((m (semantic-program ast '(()) '())))
+        (and (print-errors) m)))))
