e502fe8d027b918e738f1b895abb403faf7e4612
juleari <juleari@yandex.ru>
2016-08-05 04:14:49 +0300

    test remove

--- mllang/setup.py
+++ mllang/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='mlscheme',
+setup(name='testmlscheme',
       packages=find_packages(),
       entry_points = {
         'console_scripts': [
