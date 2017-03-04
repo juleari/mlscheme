897244427d28297caea087bcc11f73104816f162
juleari <juleari@yandex.ru>
2017-03-04 18:56:02 +0300

    add version

--- mllang/setup.py
+++ mllang/setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
+import mlscheme
 
-setup(name='mlscheme',
-      packages=find_packages(),
-      include_package_data=True,
-      entry_points = {
+setup(
+    name='mlscheme',
+    version=mlscheme.__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points = {
         'console_scripts': [
             'mlscheme = mlscheme.compiler:main',
         ]
-      },
+    },
 )
